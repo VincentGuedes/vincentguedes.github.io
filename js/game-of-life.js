dc38d95d3f613e97cae198d60856c2a833afe84c
vguedes <vguedes@d-edge.com>
2020-07-09 14:12:13 +0200

    add game of life implementation

--- js/game-of-life.js
+++ js/game-of-life.js
@@ -48,13 +48,13 @@ function makeColumns(cellNum) {
 function toggleCell(rowNum, colNum) {
     console.log("rowNum", rowNum)
     console.log("colNum", colNum)
-    grid[rowNum][colNum] = grid[rowNum][colNum] ? 0 : 1; 
+    grid[rowNum][colNum] = grid[rowNum][colNum] ? 0 : 1;
     document.getElementById(`cell-${rowNum}-${colNum}`).className = grid[rowNum][colNum] ? "cell alive" : "cell";
 }
 
 function playGame() {
     console.log("launched game");
-    gameTimer = setInterval(updateGrid, 500);
+    gameTimer = setInterval(updateGrid, 100);
 }
 
 function pauseGame() {
@@ -64,7 +64,7 @@ function pauseGame() {
 
 function updateGrid() {
 
-    let gridCopy = grid.map(function(arr) {
+    let gridCopy = grid.map(function (arr) {
         return arr.slice();
     });
 
@@ -72,8 +72,24 @@ function updateGrid() {
         for (let j = 0; j < grid[0].length; j++) {
             // update rule: alive => dead; dead => alive
             // grid[i][j] = (grid[i] && grid[i][j]) ? 0 : 1;
-            grid[i][j] = Math.floor(Math.random()*2);
+            const nbNeighbours = numberOfNeighbours(i,j);
+            if(grid[i][j]){
+              grid[i][j] = (nbNeighbours == 2 || nbNeighbours == 3);  
+            } else{
+                grid[i][j] = nbNeighbours == 3;
+            }
             document.getElementById(`cell-${i}-${j}`).className = grid[i][j] ? "cell alive" : "cell";
         }
+    }
+
+    function numberOfNeighbours(i, j) {
+        const i_minus_1 = i > 0 ? i - 1 : gridCopy.length - 1;
+        const j_minus_1 = j > 0 ? j - 1 : gridCopy[0].length - 1;
+        const i_plus_1 = i < gridCopy.length - 1 ? i + 1 : 0;
+        const j_plus_1 = j < gridCopy[0].length - 1 ? j + 1 : 0;
+
+        return gridCopy[i_minus_1][j_minus_1] + gridCopy[i_minus_1][j] + gridCopy[i_minus_1][j_plus_1] +
+               gridCopy[i][j_minus_1] + gridCopy[i][j_plus_1] +
+               gridCopy[i_plus_1][j_minus_1] + gridCopy[i_plus_1][j] + gridCopy[i_plus_1][j_plus_1];
     }
 }
